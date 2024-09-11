c342b6fe91245f07d904d92d0369b2f8b4790ef3
Der324 <ntalenderick@gmail.com>
2024-09-11 10:34:25 +0300

    Practicing python py4e with Mr.Chuck Online.

---
+++ Dictionaries C.py
@@ -0,0 +1,29 @@
+
+name = input('Enter file:')
+handle = open(name)
+
+counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word,0) + 1
+        print(counts)
+
+
+bigcount = None
+bigword = None
+for word, count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+print(bigword, bigcount)
+
+
+
+# Exercise
+#what is the following code print?:
+
+counts = { 'chuck' : 1 , 'annie' : 42, 'jan': 100}
+for key in counts:
+    if counts[key] > 10:
+        print(key, counts[key])
---
+++ Dictionaries&Loops.py
@@ -0,0 +1,35 @@
+#counting pattern
+counts = dict()
+print('Enter a line of text:')
+line = input('')
+
+words = line.split()
+
+print('Words:', words)
+
+print('Counting...')
+for word in words:
+    counts[word] = counts.get(word,0) + 1
+print('Counts', counts)
+
+#Definte Loops & Dictionaries
+
+counts = {'chuck':1, 'fred':42, 'jan':100}
+for key in counts:
+    print(key, counts[key])
+
+
+#Retrieving Lists of Keys and Values
+
+jjj = {'chuck':1, 'fred':42, 'jan':100}
+print(list(jjj))
+
+print(jjj.keys())
+print(jjj.values())
+print(jjj.items())
+
+#Two Iteration Variables
+
+jjj = {'chuck':1, 'fred':42, 'jan':100}
+for aaa,bbb in jjj.items() :
+    print(aaa, bbb)
